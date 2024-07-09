c95967da4d4d82000ea0d9992569b8ec5bbc1b9a
fy-st0rm <slokpradhan2062@gmail.com>
2024-07-10 00:46:03 +0545

    added guard sprite

--- scenes/game.py
+++ scenes/game.py
@@ -3,29 +3,34 @@ from engine import *
 
 
 class Guard:
-	def __init__(self, pos: tuple[int, int], delay: int, fov: int, radius: int):
+	def __init__(self, img: pygame.Surface, pos: tuple[int, int], delay: int, fov: int, radius: int):
+		self.img = img
 		self.delay = delay
 		self.fov = fov
 		self.radius = radius
 
 		self.dir = "right"
-		self.rect = pygame.Rect(pos[0], pos[1], 50, 100)
+		self.rect = pygame.Rect(pos[0], pos[1], img.get_width(), img.get_height())
 		self.start = False
 		self.start_time = time.time()
 		self.color = [255, 255, 0]
 
 	def update(self, surface: pygame.Surface, dt: float):
 		self.__cal_delay()
-		pygame.draw.rect(surface, [255, 0, 0], self.rect)
+		self.__cal_walk()
 
-		mp = pygame.mouse.get_pos()
+		surface.blit(self.img, (self.rect.x, self.rect.y))
+
+		mp = list(pygame.mouse.get_pos())
+		mp[0] = mp[0] / WIN_WIDTH * 266
+		mp[1] = mp[1] / WIN_HEIGHT * 200
 
 		if self.dir == "right":
-			head = [self.rect.x + 50 / 2 + 25, self.rect.y + 20]
+			head = [self.rect.x + self.rect.w / 2, self.rect.y]
 			half_ang = (self.fov / 2) * (math.pi / 180)
 			ang = math.atan2(mp[1] - head[1], mp[0] - head[0]) * 180 / math.pi
 		else:
-			head = [self.rect.x, self.rect.y + 20]
+			head = [self.rect.x + self.rect.w / 2, self.rect.y]
 			half_ang = math.pi - ((self.fov / 2) * (math.pi / 180))
 			ang = math.atan2(head[1] - mp[1], head[0] - mp[0]) * 180 / math.pi
 
@@ -50,9 +55,20 @@ class Guard:
 		if time.time() - self.start_time > self.delay:
 			if self.dir == "right":
 				self.dir = "left"
+				self.img = pygame.transform.flip(self.img, True, False)
 			else:
 				self.dir = "right"
+				self.img = pygame.transform.flip(self.img, True, False)
 			self.start_time = time.time()
+
+	def __cal_walk(self):
+		if time.time() - self.start_time >= self.delay / 3:
+			return
+
+		if self.dir == "right":
+			self.rect.x += 1
+		else:
+			self.rect.x -= 1
 
 
 class Game(Scene):
@@ -60,7 +76,10 @@ class Game(Scene):
 		self.surface = surface
 		self.scene_manager = scene_manager
 
-		self.guard1 = Guard((WIN_WIDTH / 2, WIN_HEIGHT / 2), 2, 90, 300)
+		self.game_surface = pygame.Surface((266, 200))
+
+		guard_img = pygame.image.load("assets/Guard.png")
+		self.guard1 = Guard(guard_img, (266 / 2, 200 / 2), 5, 90, 300)
 
 	def on_entry(self):
 		print("Entered game")
@@ -74,8 +93,14 @@ class Game(Scene):
 				self.scene_manager.switch("menu")
 
 	def on_update(self, dt: float):
-		# self.game_surface.fill((165, 165, 165))
-		self.surface.fill((165, 165, 165))
+		self.game_surface.fill((165, 165, 165))
+		self.guard1.update(self.game_surface, dt)
 
-		self.guard1.update(self.surface, dt)
+		self.surface.blit(
+			pygame.transform.scale(
+				self.game_surface,
+				(self.surface.get_width(), self.surface.get_height())
+			),
+			(0, 0)
+		)
 
